a1660999356b5b92373fe64c3d1c3d52e6641cb6
Ricardo Dulanto <61167218+rdulanto@users.noreply.github.com>
2022-09-01 08:39:58 -0500

    Add files via upload

---
+++ POO2_S3_S1_1/src/poo2_s3_s1_1/POO2_S3_S1_1.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo2_s3_s1_1;
+
+/**
+ *
+ * @author profesorc2laba705
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class POO2_S3_S1_1 {
+
+
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame("Ejemplo de boton");
+        
+        //textField
+        JTextField tf = new JTextField();
+        tf.setBounds(50, 50, 150, 20);
+        
+        //Button
+        String ruta="D:\\profesorc2laba705\\Documents\\NetBeansProjects\\POO2_S3_S1_1\\img\\imagen1.jpg";
+        JButton b = new JButton(new ImageIcon("img\\imagen1.jpg"));
+        
+        b.setBounds(100, 100, 100, 100);
+        
+        b.addActionListener(new ActionListener(){
+            
+            public void actionPerformed(ActionEvent e){
+                tf.setText("Bienvenidos a POO2");
+            }
+        
+        });
+        
+        
+        f.add(b);
+        f.add(tf);
+        
+        f.setSize(400,400);
+        f.setLayout(null);
+        
+        f.setVisible(true);
+        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    
+}
