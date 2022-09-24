0003db3decad4a108de845e3e6daa00298d78637
Ricardo Dulanto <61167218+rdulanto@users.noreply.github.com>
2022-09-24 08:05:46 -0500

    Add files via upload

--- S6_S1_Collections/src/s6_s1_collections/cArrayList.java
+++ S6_S1_Collections/src/s6_s1_collections/cArrayList.java
@@ -23,6 +23,66 @@ public class cArrayList {
         //manipular el elemento
         frutas.set(2, "Uva");
         
+        
+        //elimniar el elemento
+        frutas.remove("Arandano");
+        System.out.println("Se elimino Arandano");
+        
+        frutas.remove(0);
+        System.out.println("Se elimino el primer elemento");
+        for(String fruta:frutas)
+            System.out.println(fruta);
+        
+        
+        
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        
+        
+        for(Integer numero:numeros)
+            System.out.println(numero);
+        
+        numeros.remove((Integer)1);
+        System.out.println("Eliminando numeros");
+        for(Integer numero:numeros)
+            System.out.println(numero);
+        
+        
+        ArrayList<Integer> misNumeros = new ArrayList<>();
+        misNumeros.add(4);
+        misNumeros.add(5);
+        misNumeros.add(6);
+        
+        //Agregando elementos de numeros
+        misNumeros.addAll(numeros);
+        
+        Collections.sort(misNumeros);
+        
+        System.out.println("Mostrando la nueva lista de misNumeros");
+        for(Integer numero:misNumeros)
+            System.out.println(numero);
+        
+        //eliminado los elementos que pertenecen a numeros
+        misNumeros.removeAll(numeros);
+        
+        
+        System.out.println("eliminado los elementos que pertenecen a numeros");
+        for(Integer numero:misNumeros)
+            System.out.println(numero);
+        
+        //misNumeros.removeIf(str -> {str.contains(4);});
+        
+        misNumeros.clear(); 
+        System.out.println("Eliminando todos los elementos de la lista");
+        for(Integer numero:misNumeros)
+            System.out.println(numero);
+        
+        
+        System.out.println("¿La lista esta vacia? " + 
+                misNumeros.isEmpty());
+        /*
         //Ordenando la lista
         Collections.sort(frutas);
         
@@ -37,7 +97,7 @@ public class cArrayList {
             String miFruta = lst.previous();
             System.out.println("La fruta es: " + miFruta);
         }
-        */
+        
         
         for(int i=0; i<frutas.size();i++){
             System.out.println("La fruta ahora es: " + frutas.get(i));
@@ -50,7 +110,7 @@ public class cArrayList {
             System.out.println("La fruta ahora es: " + frutas.get(i));
         }
         
-        
+       
         //utilizando la clase Alumnos
         
         Alumno al1 = new Alumno(1001, "Carlos", 22);
@@ -71,7 +131,7 @@ public class cArrayList {
             System.out.println("El alumno " + al.nombre + " con codigo " + 
                     al.codigo + " tiene " + al.edad + " años.");
         }
-        
+         */
     }
     
     
